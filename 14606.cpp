a6e8abdfb348d3ec5372772316d4582b035e8ac5
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2022-06-22 17:38:36 +0900

    Add files via upload

---
+++ 14606.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+typedef pair<int, int>P;
+
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	stack <int> s;
+	int N;
+	cin >> N;
+	
+	int arr[11] = {};
+	
+	for (int i = 2; i <= N; i++) {
+		arr[i] = i - 1 + arr[i - 1];
+	}
+
+	cout << arr[N];
+
+}
