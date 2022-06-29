5679f54eaaeb2bf25f0608e8dff9186aa635bfa1
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2022-06-29 20:19:13 +0900

    Add files via upload

---
+++ 2293.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include <string>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+
+    int n, k;
+
+    cin >> n >> k;
+    int arr[101];
+    for (int i = 0; i < n; i++)cin >> arr[i];
+
+    //assert
+    if (n == 2)cout << "@@@@@@@@@@";
+
+    int dp[10001] = {};
+    dp[0] = 1;
+
+    //i번째 동전을 쓸 차례
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j <= k; j++) {
+            if (j - arr[i] >= 0)dp[j] += dp[j - arr[i]];
+        }
+    }
+
+    cout << dp[k];
+    
+
+
+}
