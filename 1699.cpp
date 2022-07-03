6916e9ac695d7db521b80d078e15f38cfc756fb7
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2022-07-03 12:15:20 +0900

    Add files via upload

---
+++ 1699.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include <string>
+
+using namespace std;
+
+
+int dp[100001];
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+
+    int N; cin >> N;
+    
+    dp[0] = 0;
+    for (int i = 1; i <= N; i++) {
+        dp[i] = i;
+        for (int j = 2; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+
+
+    cout << dp[N];
+    
+  
+
+
+
+}
