c70f8e5270ce3a5d41b418227b4110c2c3b5ee7e
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2022-07-16 12:45:23 +0900

    Add files via upload

---
+++ 25083.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<unordered_map>
+#include<cstring>
+#include<math.h>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+
+
+/*
+
+
+
+
+
+
+*/
+
+int main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+	
+
+
+
+}
