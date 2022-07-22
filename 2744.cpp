1a9cbef5fcc2f93e0a1d0acd5ed6e0483fdeaffa
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2022-07-22 16:04:35 +0900

    Add files via upload

---
+++ 2744.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include <string>
+
+using namespace std;
+
+
+int dp[1001];
+
+
+
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    char a[101];
+    cin >> a;
+    
+    for (int i = 0;a[i]!='\0'; i++) {
+        
+        if (a[i] >= 'A' && a[i] <= 'Z')cout << (char)(a[i]-('A'-'a'));
+        else cout << (char)(a[i] + ('A' - 'a'));
+
+
+    }
+ 
+
+    
+}
