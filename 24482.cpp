1c771f4b6043f5aba85846a91e5e41ea5878a772
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2022-08-08 16:42:51 +0900

    Add files via upload

---
+++ 24482.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+int N, M, R;
+vector <int> adj[100001];
+int visited[100001];
+int Count = 0;
+
+void dfs(int node) {
+
+    for (auto next : adj[node]) {
+        if (visited[next] != -1)continue;
+        visited[next] = visited[node] + 1;
+        dfs(next);
+    }
+}
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> M >> R;
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    fill(&visited[0], &visited[100001], -1);
+
+    for (int i = 0; i <= N; i++) {
+        sort(adj[i].begin(), adj[i].end(), cmp);
+    }
+
+    visited[R] = Count++;
+    dfs(R);
+
+
+    for (int i = 1; i <= N; i++) {
+        cout << visited[i] << "\n";
+    }
+
+}
