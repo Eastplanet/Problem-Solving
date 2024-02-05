f8e579b8ea829d9c0dd00bd79634efd6af5dbc91
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-02-05 21:16:27 +0900

    [Gold IV] Title: RGB거리 2, Time: 132 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -27,7 +27,7 @@ public class Main {
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<3;j++) {
 				for(int k=0;k<3;k++) {
-					dp[i][j][k] = 2000000;
+					dp[i][j][k] = 10000;
 				}
 			}
 		}
@@ -81,3 +81,4 @@ public class Main {
 	}
 }
 
+// 빨/초/파 //빨/초/파  ?
