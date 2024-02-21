4d10a9f26d1b98175b99cb71390a1572cf7e7b2a
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-02-21 17:15:11 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 15020 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static char[] mo = { 'a', 'e', 'i', 'o', 'u' };
+	static int[] visited;
+	static int[] word;
+	static char[] arr;
+	static int L, C;
+	
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(in.readLine());
+		arr = new char[C];
+		visited = new int[C];
+		word = new int[L];
+		for (int i = 0; i < C; i++) {
+			arr[i] = st.nextToken().charAt(0);
+		}
+		
+		Arrays.sort(arr);
+
+		back(0, 0);
+		System.out.println(sb);
+	}
+
+	public static void back(int idx, int start) {
+
+		if (idx == L) {
+			int moCount = 0;
+			int jacount = 0;
+
+			for (int i = 0; i < L; i++) {
+
+				boolean check = false;
+				for (int j = 0; j < mo.length; j++) {
+					if (mo[j] == arr[word[i]]) {
+						check = true;
+					}
+				}
+
+				if (check) {
+					moCount++;
+				} else {
+					jacount++;
+				}
+			}
+
+			if (!(moCount >= 1 && jacount >= 2)) {
+				return;
+			}
+			
+			boolean incCheck = true;
+			for(int i=0;i<L-1;i++) {
+				char now = arr[word[i]];
+				char next = arr[word[i+1]];
+				if(now-'a' <= next-'a') {
+					continue;
+				}
+				
+				incCheck = false;
+				break;
+			}
+			
+			if(incCheck == true) {
+				for(int i=0;i<L;i++) {
+					sb.append(arr[word[i]]);
+				}
+				sb.append("\n");
+			}
+
+			return;
+		}
+
+		for (int i = start; i < C; i++) {
+			if (visited[i] == 1)
+				continue;
+
+			visited[i] = 1;
+			word[idx] = i;
+			back(idx + 1,i+1);
+			visited[i] = 0;
+		}
+
+	}
+}
