1480eedea716298b13fb9e18958cb49cc120a232
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-03-10 02:02:50 +0900

    [Bronze V] Title: 초코바, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		if(N*100 >= M) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+		
+	}
+}
