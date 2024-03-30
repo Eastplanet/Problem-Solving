903074aed4da67bdc8c4fb25e451ecc2d11a044a
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-03-30 20:02:49 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int cnt = 0 ;
+		
+		while(in.readLine() != null) {
+			cnt++;
+		}
+		
+		System.out.println(cnt);
+		
+		
+	}
+}
