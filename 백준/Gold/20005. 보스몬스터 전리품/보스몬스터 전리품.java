2e670f0ab291a138f77d2ba4dbeb0bcf02e5ad49
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-03-31 17:26:19 +0900

    [Gold III] Title: 보스몬스터 전리품, Time: 348 ms, Memory: 30008 KB -BaekjoonHub

--- 백준/Gold/20005. 보스몬스터 전리품/보스몬스터 전리품.java
+++ 백준/Gold/20005. 보스몬스터 전리품/보스몬스터 전리품.java
@@ -59,10 +59,13 @@ public class Main {
 		q.add(new Node(0, bossPos));
 		visited[bossPos.y][bossPos.x] =true;
 		
+		//플레이어를 찾아 다님
 		while(!q.isEmpty() && playerCnt != P) {
 			Node cur = q.poll();
 			
-			// 초가 지났을 때
+			// 초가 지났을 때 체력 업데이트
+			// 큐에는 초가 증가순으로 쌓여 있음
+			// 0초에서 1초로 된다면 이후는 다 1초 이상임
 			if(cur.second != nowSec) {
 				nowSec = cur.second;
 				bossHP -= totalDps;
@@ -72,12 +75,12 @@ public class Main {
 				}
 			}
 			
-			
 			for(int i=0;i<4;i++) {
 				int gox = cur.pos.x + dx[i];
 				int goy = cur.pos.y + dy[i];
 				if(!canGo(gox,goy)) continue;
 				
+				//플레이어 발견( DPS에 추가 해준다.)
 				if(map[goy][gox] != '.') {
 					int dps = nameToDps.get(map[goy][gox]);
 					visited[goy][gox] = true;
@@ -85,6 +88,7 @@ public class Main {
 					totalDps += dps;
 					playerCnt ++;
 				}
+				//빈칸
 				else {
 					visited[goy][gox] = true;
 					q.add(new Node(cur.second+1 , new Pos(goy,gox)));
@@ -92,28 +96,21 @@ public class Main {
 			}
 		}
 		
-		while(true) {
-			bossHP -= totalDps;
-			if(bossHP <= 0) {
-				System.out.println(playerCnt);
-				return;
-			}
-		}
+		// 탐색이 끝나거나 플레이어를 다 찾은 경우
+		// 다찾으면 더이상 BFS하지 않고 DPS로 보스를 공격
+		System.out.println(playerCnt);
+		
 		
 	}
 
 	static void init() throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		
-
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		P = Integer.parseInt(st.nextToken());
-
 		map = new char[R][C];
 		visited = new boolean[R][C];
-
 		for (int i = 0; i < R; i++) {
 			String tmp = in.readLine();
 			for (int j = 0; j < C; j++) {
@@ -122,10 +119,8 @@ public class Main {
 					map[i][j] = '.';
 					bossPos = new Pos(i, j);
 				}
-				
 			}
 		}
-
 		nameToDps = new HashMap<>();
 		for (int i = 0; i < P; i++) {
 			st = new StringTokenizer(in.readLine());
@@ -133,7 +128,6 @@ public class Main {
 			int dps = Integer.parseInt(st.nextToken());
 			nameToDps.put(name, dps);
 		}
-
 		bossHP = Integer.parseInt(in.readLine());
 	}
 }
