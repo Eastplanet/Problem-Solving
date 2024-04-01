f5f0be94822661eda9232195597789f0c0721dd5
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-04-01 11:29:54 +0900

    [Gold V] Title: AC, Time: 764 ms, Memory: 80624 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -5,6 +5,10 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static Deque<Integer> dq;
+	
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -17,14 +21,12 @@ public class Main {
 			String substring = data.substring(1, data.length() - 1);
 			String[] splits = substring.split(",");
 
-			Deque<Integer> dq = new ArrayDeque<>();
+			dq = new ArrayDeque<>();
 			for (int i = 0; i < N; i++) {
 				dq.add(Integer.parseInt(splits[i]));
 			}
-
 			boolean isReverse = false;
 			boolean isError = false;
-			
 			for (int i = 0; i < cmd.length(); i++) {
 				char c = cmd.charAt(i);
 				if (c == 'R') {
@@ -35,23 +37,17 @@ public class Main {
 						isError = true;
 						break;
 					}
-					if (isReverse) {
-						dq.pollLast();
-					} else {
-						dq.pollFirst();
-					}
+					pollUsingFlag(isReverse);
 				}
 			}
-			if(isError) {
+			if (isError) {
 				continue;
 			}
 			sb.append("[");
 			while (!dq.isEmpty()) {
-				if (isReverse) {
-					sb.append(dq.pollLast());
-				} else {
-					sb.append(dq.pollFirst());
-				}
+				
+				sb.append(pollUsingFlag(isReverse));
+				
 				if (dq.size() != 0) {
 					sb.append(",");
 				}
@@ -59,5 +55,13 @@ public class Main {
 			sb.append("]").append("\n");
 		}
 		System.out.println(sb);
+	}
+	
+	static int pollUsingFlag(boolean isReverse) {
+		if (isReverse) {
+			return dq.pollLast();
+		} else {
+			return dq.pollFirst();
+		}
 	}
 }
