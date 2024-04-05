9bb8872ed31eb8671d3afa682b0162c66fe330e2
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-04-05 10:01:30 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 120 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(in.readLine());
+		int b = Integer.parseInt(in.readLine());
+		
+		System.out.println(a+b+3);
+	}
+}
