ccdb02295f57e980c9aa880e5e3796b3025c5b70
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-04-28 11:52:48 +0900

    [Bronze V] Title: A+B, Time: 192 ms, Memory: 15688 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		BigInteger A = new BigInteger(in.readLine());
+		BigInteger B = new BigInteger(in.readLine());
+		System.out.println(A.add(B));
+
+	}
+}
