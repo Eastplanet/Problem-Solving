275f9badcfdb36823220667b03218695e7854fbb
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-06-16 13:53:27 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		System.out.println((int)(N*((double)10/11)));
+	}
+}
