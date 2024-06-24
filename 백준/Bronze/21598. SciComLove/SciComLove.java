54baa05e0d63cd6b74952e14c750cda04aab5444
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-06-25 07:05:04 +0900

    [Bronze V] Title: SciComLove, Time: 124 ms, Memory: 14088 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		for(int i=0;i<N;i++) {
+			System.out.println("SciComLove");
+		}
+	}
+}
