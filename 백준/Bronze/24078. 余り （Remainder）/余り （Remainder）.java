fbc609d0ba6a236c696eecce22aed7212ec09b70
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-07-02 12:09:20 +0900

    [Bronze V] Title: 余り (Remainder), Time: 96 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+        System.out.println(N%21);
+	}
+
+}
