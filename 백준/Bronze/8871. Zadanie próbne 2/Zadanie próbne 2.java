1aede6f0d6d5159223564ac4451c074cc48034d0
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-07-07 16:18:26 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 148 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		System.out.println((N+1)*2 +" "+(N+1)*3);
+	}
+
+}
