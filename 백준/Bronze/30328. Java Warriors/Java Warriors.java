46d34cdcc367ae17b97bd29459a504cb688dbec8
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-07-18 21:30:50 +0900

    [Bronze V] Title: Java Warriors, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		int a = Integer.parseInt(in.readLine());
+		System.out.println(a*4000);
+	}
+
+}
