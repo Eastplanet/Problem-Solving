7afc0407a4e2938308e10c4a714c976e9119ab62
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-07-21 22:32:13 +0900

    [Bronze V] Title: 時間 (Hour), Time: 124 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+    
+    public static void main(String[] args) throws Exception{
+      int N = Integer.parseInt(in.readLine());
+      System.out.println(N*24);
+    }
+}
