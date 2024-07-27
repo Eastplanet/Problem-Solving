b24944ad1a6dd018fe4459d152df1cd2fb8f5531
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-07-27 14:38:38 +0900

    [Bronze V] Title: Metronome, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+    
+    public static void main(String[] args) throws Exception{
+
+    	int N = Integer.parseInt(in.readLine());
+    	System.out.println((double)N/4);
+    }
+}
