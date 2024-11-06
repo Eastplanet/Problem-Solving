20b4c45b50c7bc7bad7e5ef806bfb461465eb242
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2024-11-06 23:57:52 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 112 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static StringTokenizer st;
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		
+		int T = Integer.parseInt(in.readLine());
+		while(T-- > 0) {
+			System.out.print("LoveisKoreaUniversity ");
+		}
+		
+	}
+	
+}
