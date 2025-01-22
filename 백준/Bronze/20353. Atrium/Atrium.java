1205951f1c43566c1e4a050035f146cd393f4da2
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2025-01-22 22:04:42 +0900

    [Bronze IV] Title: Atrium, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	public static StringTokenizer st;
+
+
+	public static void main(String[] args) throws Exception {
+
+		long a = Long.parseLong(in.readLine());
+		
+		System.out.println(Math.sqrt(a)*4);
+	}
+	
+
+
+
+
+}
