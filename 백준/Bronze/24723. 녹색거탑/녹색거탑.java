e635d9010ea4e50e91b3a9b6a46e3764c8661de1
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2025-02-15 17:17:13 +0900

    [Bronze IV] Title: 녹색거탑, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	public static StringTokenizer st;
+	
+	public static void main(String[] args) throws Exception {
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		
+		System.out.println((int)Math.pow(2, N));
+	}
+
+	
+}
