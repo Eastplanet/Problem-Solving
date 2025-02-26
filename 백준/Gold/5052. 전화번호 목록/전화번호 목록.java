fc29dd251960d12f34c6d838c4421f5958b4f009
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2025-02-26 17:04:03 +0900

    [Gold IV] Title: 전화번호 목록, Time: 300 ms, Memory: 107552 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,73 +1,107 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = Integer.parseInt(in.readLine());
-		while (T-- > 0) {
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    public static int N;
+    public static String[] arr;
 
-			int N = Integer.parseInt(in.readLine());
-			Set<String> banSet = new HashSet<>();
-			Set<String> realSet = new HashSet<>();
-			boolean okFlag = true;
+    public static class Trie {
+        Trie[] child;
+        // 여기서 끝나는 전화번호가 있나요
+        boolean isOutput;
+        // 자식이 있나요
+        boolean goExist;
+        char now;
 
-			String[] numList = new String[N];
-			for (int i = 0; i < N; i++) {
-				numList[i] = in.readLine();
-			}
+        public Trie(){
+            child = new Trie[10];
+        }
 
-			for (int i = 0; i < N; i++) {
-				// 1234가 일관성이 있는지 확인
-				String num = numList[i];
 
-				// 1. 1234가 banSet에 포함되어있는지 확인 ( ex. 12345에 의해 1234가 못들어감 )
-				if (banSet.contains(num)) {
-					okFlag = false;
-				}
+        public void makeTrie(String num , int idx){
 
-				// 2. 1, 12, 123, 1234(두 전화번호가 같은 겨우는 없긴 함) 라는 숫자가 realSet에 포함되어 있는지 확인 ( ex.
-				// 1234에 의해 1,12,123이 못들어감 )
-				String[] list = partition(num);
-				for (String item : list) {
-					if (realSet.contains(item)) {
-						okFlag = false;
-					}
-				}
+            // 끝이 아닌 경우 재귀
+            if(idx + 1 < num.length()){
 
-				if (okFlag == false) {
-					break;
-				}
-				
-				// banSet에 1,12,123,1234 넣기, realSet에 1234넣기
-				for (String item : list) {
-					banSet.add(item);
-				}
-				realSet.add(num);
+                if(child[num.charAt(idx+1)-'0'] != null){
+                    child[num.charAt(idx+1)-'0'].makeTrie(num, idx + 1);
+                }
+                else{
+                    Trie t = new Trie();
+                    t.makeTrie(num,idx + 1);
+                    child[num.charAt(idx+1)-'0'] = t;
+                }
 
-			}
+                if(idx != -1){
+                    now = num.charAt(idx);
+                }
+                goExist = true;
+            }
+            // 끝인 경우
+            else {
+                isOutput = true;
+            }
+        }
 
-			if (okFlag) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
-		}
+        public boolean recursiveFindInvalid(){
+            // 일관성 없음 !
+            if(isOutput && goExist){
+                return true;
+            }
 
-	}
+            if(goExist){
+                boolean ans = false;
+                for(Trie t : child){
+                    if(t == null){
+                        continue;
+                    }
+                    ans = ans | t.recursiveFindInvalid();
+                }
+                return ans;
+            }
 
-	// 1234가 들어오면 [1, 12, 123, 1234]로 나눠준다.
-	static String[] partition(String num) {
-		
-		String[] result = new String[num.length()];
-		
-		for (int i = 0; i < num.length(); i++) {
-			result[i] = num.substring(0, i+1);
-		}
+            return false;
+        }
 
-		return result;
-	}
+
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        int T = Integer.parseInt(in.readLine());
+        while(T-- > 0){
+            init();
+
+            Trie root = new Trie();
+            for(int i=0;i<arr.length;i++){
+                root.makeTrie(arr[i],-1);
+            }
+
+            if(root.recursiveFindInvalid()){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
+
+        }
+
+
+    }
+
+    public static void init() throws Exception{
+        N = Integer.parseInt(in.readLine());
+        arr = new String[N];
+        for(int i=0;i<N;i++){
+            arr[i] = in.readLine();
+        }
+    }
+
+
+
+
 }
