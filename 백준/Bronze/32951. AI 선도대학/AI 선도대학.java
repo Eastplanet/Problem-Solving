39bc9d75139b430a0bb0850952424584c6e24a61
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2025-03-12 20:37:00 +0900

    [Bronze V] Title: AI 선도대학, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	public static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception {
+		int N = Integer.parseInt(in.readLine());
+		System.out.println(N-2024);
+	}
+	
+	
+
+	
+
+}
