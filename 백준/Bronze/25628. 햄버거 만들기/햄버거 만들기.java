601c1bc144c188489ed40d00418867c4ff432be5
Eastplanet <73326589+Eastplanet@users.noreply.github.com>
2025-04-27 21:58:29 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	public static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		st = new StringTokenizer(in.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		A /= 2;
+		
+		System.out.println((Integer)Math.min(A,B));
+		
+		
+	}
+	
+
+
+}
